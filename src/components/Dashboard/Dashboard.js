7d234dad94164036d3f6dafe372b293df1c7207f
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-30 12:30:16 -0600

    started adding sass to project

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import './Dashboard.css';
 
 export default class Dashboard extends Component {
   render() {
     return(
-      <div>Dashboard Component</div>
+      <div className='dashboard'>
+        <h1>Color Palette Testing...</h1>
+        <div className='text-container'>
+        <h1>This is a text container</h1>
+        <p>
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.
+          Look at all this text. It's amazing. Filler filler filler.          
+        </p>
+        <button className='button1'>Button #1</button>
+        <button className='button2'>Button #2</button>
+        </div>
+      </div>
     );
   }
 }
