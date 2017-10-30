57d9b9cf52c9356f4a992539dc6d1a8988f3af48
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-30 16:21:11 -0600

    began styling dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,42 +1,23 @@
 import React, { Component } from 'react';
-import './Dashboard.css';
+import './Dashboard.css'
 
 export default class Dashboard extends Component {
   render() {
     return(
-      <div className='dashboard'>
-        <h1>Color Palette Testing...</h1>
-        <div className='text-container'>
-        <h1>This is a text container</h1>
-        <p>
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.
-          Look at all this text. It's amazing. Filler filler filler.          
-        </p>
-        <button className='button1'>Button #1</button>
-        <button className='button2'>Button #2</button>
+    <div className='dashboard'>
+      <div className='dashboard-contents'>
+        <div className='header'>
+          <h1>Bread Crumb</h1>        
+          <p>Start your journey. Create and manage scavenger hunts in the dashboard.</p>
+        </div>
+        <div className='container scav-hunts'>
+          <p>SCAV</p>
+        </div>
+        <div className='container new-scav-hunt'>
+          <input placeholder='create new scav hunt'/>
         </div>
       </div>
+    </div>
     );
   }
 }
