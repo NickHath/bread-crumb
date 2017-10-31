625e80d2d8c04af58a63ecae85ff69cb20f34295
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-30 20:24:29 -0600

    wrote DB schema

---
+++ schema/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE accounts (
+  account_id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(40),
+  last_name VARCHAR(40)
+)
+
+CREATE TABLE scavenger_hunts (
+  hunt_id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(40),
+  description VARCHAR(1000),
+  account_id INTEGER,
+  FOREIGN KEY(account_id) REFERENCES accounts(account_id)
+)
+
+CREATE TABLE recipients (
+  recipient_id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(40),
+  last_name VARCHAR(40),
+  phone VARCHAR(20),
+  hunt_id INTEGER,
+  FOREIGN KEY(hunt_id) REFERENCES scavenger_hunts(hunt_id)
+)
+
+CREATE TABLE tasks (
+  task_id SERIAL PRIMARY KEY NOT NULL,
+  task VARCHAR(255),
+  hint VARCHAR(255),
+  answer VARCHAR(255),
+  hunt_id INTEGER,
+  FOREIGN KEY(hunt_id) REFERENCES scavenger_hunts(hunt_id)
+)
