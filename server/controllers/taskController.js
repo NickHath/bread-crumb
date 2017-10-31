4e5781f629a934f99266e6a85be7788d9da5f667
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-31 14:58:23 -0600

    wrote and tested taskController.js

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,8 +1,8 @@
 module.exports = {
   getTasks: function(req, res) {
     const db = req.app.get('db');
-    db.QUERY()
-      .then(() => res.status(200).send())
+    db.tasks.get_tasks()
+      .then(tasks => res.status(200).send(tasks))
       .catch(err => res.status(500).send(err));
 
   },
@@ -10,32 +10,34 @@ module.exports = {
 
   getTask: function(req, res) {
     const db = req.app.get('db');
-    db.QUERY()
-      .then(() => res.status(200).send())
+    db.tasks.get_task_by_id([req.params.id])
+      .then(recipient => res.status(200).send(recipient))
       .catch(err => res.status(500).send(err));
   },
 
 
   createTask: function(req, res) {
     const db = req.app.get('db');
-    db.QUERY()
-      .then(() => res.status(200).send())
+    const { task, hint, answer, hunt_id } = req.body;
+    db.tasks.create_task([task, hint, answer, hunt_id])
+      .then(() => res.status(200).send(`Created new task`))
       .catch(err => res.status(500).send(err));
   },
 
 
   editTask: function(req, res) {
     const db = req.app.get('db');
-    db.QUERY()
-      .then(() => res.status(200).send())
+    const { task, hint, answer } = req.body;
+    db.tasks.edit_task([req.params.id, task, hint, answer])
+      .then(() => res.status(200).send(`Edited task ${req.params.id}`))
       .catch(err => res.status(500).send(err));
   },
 
 
   deleteTask: function(req, res) {
     const db = req.app.get('db');
-    db.QUERY()
-      .then(() => res.status(200).send())
+    db.tasks.delete_task([req.params.id])
+      .then(() => res.status(200).send(`Deleted task ${req.params.id}`))
       .catch(err => res.status(500).send(err));
   }
 }
