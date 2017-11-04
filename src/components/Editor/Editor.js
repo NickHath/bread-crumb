fdfa241b0bbd2896919055ade38d08d7ee630d0e
Nick Hathaway <Nicholashathaway@gmail.com>
2017-11-04 17:43:18 -0600

    wrote boilerplate JSX and scss for Editor component

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
+// material ui
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import styles from './EditorMuiStyles';
+
 class Editor extends Component {
 
   render() {
     return(
-      <div>Editor Component</div>      
+      <div className='editor-wrapper'>
+        <div className='editor'>
+          <div className='container editor-contents'>
+            <h1>Editor</h1>
+            <div className='editor-input'>
+              <h2>Recipients</h2>
+              <TextField underlineFocusStyle={styles.underlineFocusStyle}
+                         ref='first'/>
+            </div>
+            <div className='editor-input'>
+              <h2>Task #1</h2>
+              <TextField underlineFocusStyle={styles.underlineFocusStyle}
+                         ref='last'/>
+            </div>
+            <div className='editor-input'>
+              <h2>Task #2</h2>
+              <TextField underlineFocusStyle={styles.underlineFocusStyle}
+                         ref='last'/>
+            </div>
+          </div>
+        </div>
+      </div>      
     )
   }
 }
---
+++ src/components/Editor/EditorMuiStyles.js
@@ -0,0 +1,10 @@
+import { amber500 } from 'material-ui/styles/colors';
+
+export default {
+  buttonStyle: {
+    margin: 12
+  },
+  underlineFocusStyle: {
+    borderColor: amber500
+  }
+}
